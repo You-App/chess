9d713f1f0a5e8a4be1ebcb7c21654ce68ab31d71
You-App <54545296+You-App@users.noreply.github.com>
2023-10-21 18:28:50 +0300

    Update board-init.js

--- js/board-init.js
+++ js/board-init.js
@@ -1,346 +1,372 @@
-var board,
-  game,
-  gameTimer,
-  gameStarted = false,
-  playerSide = 'w',
-  opponentSide = 'b',
-  firstTurn = 'player',
-  promotionPos,
-  moveSource,
-  moveTarget,
-  promotionFigure = 'q',
-  promotionEvent,
-  togglePlayer = false,
-  engineDisabled = false,
-  engineSkill = 8,
-  staticSkill = 16,
-  stateAnalyze = false,
-  stateAnalyzeMatch = '',
-  stateHint = false,
+var autoPromotion = true; // auto promotion flag
+var showHint = true; // show hint flag
+var analyzeMode = false; // analyze mode flag
+
+// Function to start the game
+function startGame() {
+  gameStarted = true;
+  game.reset();
+  board.start();
+  stopTimer();
+  resetGame();
+  startTimer();
+
+  // Reset flags and variables
   gameEnd = false;
+  engineDisabled = false;
+  stateAnalyze = false;
+  stateAnalyzeMatch = '';
+  stateHint = false;
 
-var stockfishMoves = localStorage.getItem('stockfishMoves');
+  // Clear log
+  dumpLog();
 
-if (stockfishMoves) {
-  stockfishMoves = JSON.parse(stockfishMoves);
-} else {
-  stockfishMoves = [];
-}
+  // Update UI
+  $('#btn-take-back').addClass('disabled');
+  $('#game-analyze-string').text('').addClass('hidden');
+  $('#game-turns-history ol').html('');
 
-function dumpLog(data) {
-  if (!data) {
-    $('.chess-log').html('');
-  } else {
-    $('.chess-log').html($('.chess-log').html() + data + '<br />');
-  }
-}
+  // Generate initial list of moves
+  listMoves();
 
-function listMoves() {
-  var movesArray = game.history();
-  var movesHtml = '';
-  var turnFrom = 1;
-  for (i = 0; i < movesArray.length; i = i + 2) {
-    if (movesArray[i + 1] == undefined) {
-      movesHtml += '<li><span turn=' + turnFrom + '>' + movesArray[i] + '</span><span></span></li>';
-    } else {
-      movesHtml += '<li><span turn=' + turnFrom + '>' + movesArray[i] + '</span><span turn=' + (turnFrom + 1) + '>' + movesArray[i + 1] + '</span></li>';
+  // Add event listeners
+  $('#btn-show-hint').off().click(function() {
+    if (!stateHint) {
+      stateHint = true;
+      $(this).addClass('loading disabled');
+      stockfish.postMessage('go depth 10');
     }
-    turnFrom = turnFrom + 2;
-  }
-  $('#game-turns-history ol').html(movesHtml);
-  $('#game-turns-history li span').off().click(function () {
-    var turnN = parseInt($(this).attr('turn'));
-    console.log('History: show turn ' + $(this).attr('turn'));
-    moves = JSON.parse(localStorage.getItem('boardHistory'));
-    console.log(moves[turnN - 1]);
-    loadBoard(moves[turnN - 1], true);
+  });
+
+  $('#btn-analyze').off().click(function() {
+    if (!stateAnalyze) {
+      stateAnalyze = true;
+      $(this).addClass('loading disabled');
+      stockfish.postMessage('go depth ' + staticSkill);
+    }
+  });
+
+  $('#btn-take-back').off().click(function () {
+    if (!game.started() || game.turn() === firstTurn) {
+      return;
+    }
+
+    var prevFen = localStorage.getItem('boardHistory');
+    if (prevFen.length > 0) {
+      prevFen.pop();
+    }
+
+    if (prevFen.length === 0) {
+      board.start();
+      game.reset();
+      stopTimer();
+
+      // Reset flags and variables
+      gameEnd = false;
+      engineDisabled = false;
+      stateAnalyze = false;
+      stateAnalyzeMatch = '';
+      stateHint = false;
+
+      // Clear log
+      dumpLog();
+
+      // Update UI
+      $('#btn-take-back').addClass('disabled');
+      $('#game-analyze-string').text('').addClass('hidden');
+      $('#game-turns-history ol').html('');
+    } else {
+      loadBoard(prevFen[prevFen.length - 1], true);
+    }
+
+    listMoves();
+  });
+
+  $('#btn-restart').off().click(function () {
+    if (!game.started()) {
+      return;
+    }
+
+    stopTimer();
+    board.start();
+    game.reset();
+    resetGame();
+    startTimer();
+
+    // Reset flags and variables
+    gameEnd = false;
+    engineDisabled = false;
+    stateAnalyze = false;
+    stateAnalyzeMatch = '';
+    stateHint = false;
+
+    // Clear log
+    dumpLog();
+
+    // Update UI
+    $('#btn-take-back').addClass('disabled');
+    $('#game-analyze-string').text('').addClass('hidden');
+    $('#game-turns-history ol').html('');
+
+    // Generate initial list of moves
+    listMoves();
   });
 }
 
-function calcFieldNum(fieldCode) {
-  let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-  var j = 0;
-  var num = 0;
+// Function to reset the game
+function resetGame() {
+  gameStarted = false;
+  playerSide = 'w';
+  opponentSide = 'b';
+  firstTurn = 'player';
+  promotionFigure = 'q';
+  togglePlayer = false;
+  engineSkill = 8;
+  staticSkill = 16;
 
-  if (board.orientation() === 'w') {
-    for (var i = 8; i > 0; i--) {
-      letters.map(function (letter) {
-        if (letter + i === fieldCode) {
-          num = j;
-        }
-        j++;
-      });
-    }
-  } else {
-    letters = letters.reverse();
-    for (var i = 1; i <= 8; i++) {
-      letters.map(function (letter) {
-        if (letter + i === fieldCode) {
-          num = j;
-        }
-        j++;
-      });
-    }
-  }
+  // Set initial position
+  var position = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w';
 
-  return num;
+  // Load initial position
+  loadBoard(position, true);
 }
 
-stockfish.onmessage = function (event) {
-  console.log(event.data);
-  var eventStr = event.data;
+// Function to load a specific board position
+function loadBoard(fen, disableEngine) {
+  if (gameStarted) {
+    stopTimer();
+  }
 
-  if (stateHint == 'grep') {
-    var match = eventStr.match(/^bestmove ([a-h][1-8])([a-h][1-8])([qrbk])?/);
-    if (match) {
-      $('#board .square-' + match[1]).css('background', '#f7c5cd');
-      $('#board .square-' + match[2]).css('background', '#f7c5cd');
-      $('#board_chess_square_' + calcFieldNum(match[1])).css('background', '#f7c5cd');
-      $('#board_chess_square_' + calcFieldNum(match[2])).css('background', '#f7c5cd');
-      stateHint = false;
-      $('#btn-show-hint').removeClass('loading disabled');
-      setTimeout(function () {
-        $('#board .square-55d63').css('background', '');
-        $('.chess_square').css('background', '');
-      }, 2500);
-    }
+  board.position(fen);
+  game.load(fen);
+
+  // Disable engine if required
+  if (disableEngine) {
+    engineDisabled = true;
+  }
+
+  // Update UI
+  $('#board-positions-data').text('');
+  $('#btn-take-back').addClass('disabled');
+  dumpLog();
+
+  if (gameStarted) {
+    startTimer();
+  }
+}
+
+// Function to handle promotion events
+function onPromotion(event) {
+  promotionEvent = event;
+  $('#promotionModal').modal('show');
+
+  // Add event listeners for promotion options
+  $('.promotion-option').off().click(function() {
+    promotionFigure = $(this).attr('data-piece');
+    movePiece(promotionPos, moveSource, moveTarget, promotionFigure);
+    $('#promotionModal').modal('hide');
+  });
+}
+
+// Function to move a piece on the board
+function movePiece(piece, source, target, promotion) {
+  if (autoPromotion && piece.type === 'p' &&
+    ((piece.color === 'w' && target.charAt(1) === '8') ||
+    (piece.color === 'b' && target.charAt(1) === '1'))) {
+    onPromotion({source: source, target: target});
     return;
   }
 
-  if (stateAnalyze == 'grep') {
-    var regex = new RegExp("info depth " + staticSkill + " seldepth .*? pv (.*)");
-    var seldepthMatch = regex.exec(eventStr);
-    if (seldepthMatch) {
-      console.log('Analyze entry match.');
-      var moves = seldepthMatch[1].split(' ');
-      stateAnalyzeMatch = '';
-      for (var i = 0, len = 5; i < len; i++) {
-        stateAnalyzeMatch += (i + 1) + '. ' + moves[i] + ' ';
-      }
-    };
-    var regex = new RegExp("bestmove .*");
-    var bestmoveMatch = regex.exec(eventStr);
-    if (bestmoveMatch) {
-      $('#game-analyze-string').text(stateAnalyzeMatch).removeClass('hidden');
-      stateAnalyze = false;
-      $('#btn-analyze').removeClass('disabled loading');
-    }
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: promotion
+  });
+
+  if (!move) {
     return;
   }
 
-  if ((eventStr.indexOf('bestmove') + 1) && stateAnalyze == 'done') {
-    stateAnalyze = false;
+  board.position(game.fen());
+
+  checkPositions('player');
+  checkAnalyzeOption();
+  updateGameEndStatus();
+
+  if (game.history().length > 0) {
+    $('#btn-take-back').removeClass('disabled');
+  }
+
+  listMoves();
+
+  if (engineDisabled || gameEnd) {
     return;
   }
 
-  if (eventStr.indexOf('bestmove') + 1) {
-    $('#board-positions-data').text('Tactical solution: ' + event.data);
+  if (playerSide === 'w' && game.turn() === 'b' ||
+    playerSide === 'b' && game.turn() === 'w') {
+    startTimer();
+    return;
   }
 
-  if (!stateAnalyze && !stateHint) {
-    var match = eventStr.match(/^bestmove ([a-h][1-8])([a-h][1-8])([qrbk])?/);
-    if (match) {
-      console.log('Match and move.');
-      stopTimer();
-      var move = game.move({
-        from: match[1],
-        to: match[2],
-        promotion: match[3]
-      });
-      console.log(game.fen());
-      board.position(game.fen());
-      checkPositions('player');
-      checkAnalyzeOption();
-      if (game.history().length > 0) {
-        $('#btn-take-back').removeClass('disabled');
-      }
-      listMoves();
-      startTimer();
-      gameHistoryAddMove(game.fen());
-    }
-  }
-};
+  stopTimer();
 
+  var fen = game.fen();
+  var moves = board.history();
+  stockfish.postMessage('position fen ' + fen + ' moves ' + moves);
+  stockfish.postMessage('go depth ' + engineSkill);
+}
+
+// Function to start the timer
 function startTimer() {
-  if (!gameStarted) {
-    gameTimer = setInterval(function () {
-      if (playerSide == firstTurn) {
-        togglePlayer = !togglePlayer;
-        $('.chess-timer[data-player=' + (togglePlayer ? 'opponent' : 'player') + ']').children().addClass('inactive');
-        $('.chess-timer[data-player=' + (togglePlayer ? 'player' : 'opponent') + ']').children().removeClass('inactive');
-      }
-      if (gameEnd) {
-        clearTimeout(gameTimer);
-      }
-    }, 1000);
-    gameStarted = true;
+  if (gameEnd) {
+    return;
   }
+
+  var timerElement = $('#game-timer');
+
+  var timeSeconds = timerElement.attr('data-time');
+  var time = parseInt(timeSeconds, 10);
+  var minutes = Math.floor(time / 60);
+  var seconds = time - (minutes * 60);
+
+  if (!timeSeconds) {
+    timerElement.attr('data-time', 0);
+    return;
+  }
+
+  timerElement.text(('00' + minutes).slice(-2) + ':' + ('00' + seconds).slice(-2));
+
+  gameTimer = setInterval(function() {
+    if (gameStarted && !gameEnd) {
+      var currentTime = timerElement.attr('data-time');
+      var currentTimeInt = parseInt(currentTime, 10);
+      var minutes = Math.floor(currentTimeInt / 60);
+      var seconds = currentTimeInt - (minutes * 60);
+
+      if (minutes === 0 && seconds === 0) {
+        gameEnd = true;
+        timerElement.addClass('text-danger');
+        return;
+      }
+
+      if (seconds === 0) {
+        minutes--;
+        seconds = 59;
+      } else {
+        seconds--;
+      }
+
+      var newTime = ('00' + minutes).slice(-2) + ':' + ('00' + seconds).slice(-2);
+
+      timerElement.text(newTime);
+      timerElement.attr('data-time', (currentTimeInt - 1));
+
+      if (showHint && currentTimeInt === 6) {
+        stateHint = true;
+        $(this).addClass('loading disabled');
+        stockfish.postMessage('go depth 10');
+      }
+    }
+  }, 1000);
 }
 
+// Function to stop the timer
 function stopTimer() {
   clearInterval(gameTimer);
-  gameStarted = false;
+  $('#game-timer').removeClass('text-danger');
 }
 
-function gameHistoryAddMove(fen) {
-  stockfishMoves.push(fen);
-  localStorage.setItem('stockfishMoves', JSON.stringify(stockfishMoves));
-}
+// Function to check positions and update UI
+function checkPositions(player) {
+  var positions = {
+    'king': ['e1', 'e8'],
+    'checkmate': false,
+    'stalemate': false,
+    'insufficient': false,
+  };
 
-function loadBoard(fen, fromHistory) {
-  board.position(fen, fromHistory);
-  game.load(fen);
-  checkPositions(playerSide);
-  checkAnalyzeOption();
-}
-
-// Init engine
-var stockfish = new Worker('js/stockfish.js');
-
-var stockfishMoves = localStorage.getItem('stockfishMoves');
-
-if (stockfishMoves) {
-  stockfishMoves = JSON.parse(stockfishMoves);
-} else {
-  stockfishMoves = [];
-}
-
-function dumpLog(data) {
-  if (!data) {
-    $('.chess-log').html('');
-  } else {
-    $('.chess-log').html($('.chess-log').html() + data + '<br />');
-  }
-}
-
-function listMoves() {
-  var movesArray = game.history();
-  var movesHtml = '';
-  var turnFrom = 1;
-  for (i = 0; i < movesArray.length; i = i + 2) {
-    if (movesArray[i + 1] == undefined) {
-      movesHtml += '<li><span turn=' + turnFrom + '>' + movesArray[i] + '</span><span></span></li>';
-    } else {
-      movesHtml += '<li><span turn=' + turnFrom + '>' + movesArray[i] + '</span><span turn=' + (turnFrom + 1) + '>' + movesArray[i + 1] + '</span></li>';
-    }
-    turnFrom = turnFrom + 2;
-  }
-  $('#game-turns-history ol').html(movesHtml);
-  $('#game-turns-history li span').off().click(function () {
-    var turnN = parseInt($(this).attr('turn'));
-    console.log('History: show turn ' + $(this).attr('turn'));
-    moves = JSON.parse(localStorage.getItem('boardHistory'));
-    console.log(moves[turnN - 1]);
-    loadBoard(moves[turnN - 1], true);
-  });
-}
-
-function calcFieldNum(fieldCode) {
-  let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-  var j = 0;
-  var num = 0;
-
-  if (board.orientation() === 'w') {
-    for (var i = 8; i > 0; i--) {
-      letters.map(function (letter) {
-        if (letter + i === fieldCode) {
-          num = j;
-        }
-        j++;
-      });
-    }
-  } else {
-    letters = letters.reverse();
-    for (var i = 1; i <= 8; i++) {
-      letters.map(function (letter) {
-        if (letter + i === fieldCode) {
-          num = j;
-        }
-        j++;
-      });
-    }
-  }
-
-  return num;
-}
-
-stockfish.onmessage = function (event) {
-  console.log(event.data);
-  var eventStr = event.data;
-
-  if (stateHint == 'grep') {
-    var match = eventStr.match(/^bestmove ([a-h][1-8])([a-h][1-8])([qrbk])?/);
-    if (match) {
-      $('#board .square-' + match[1]).css('background', '#f7c5cd');
-      $('#board .square-' + match[2]).css('background', '#f7c5cd');
-      $('#board_chess_square_' + calcFieldNum(match[1])).css('background', '#f7c5cd');
-      $('#board_chess_square_' + calcFieldNum(match[2])).css('background', '#f7c5cd');
-      stateHint = false;
-      $('#btn-show-hint').removeClass('loading disabled');
-      setTimeout(function () {
-        $('#board .square-55d63').css('background', '');
-        $('.chess_square').css('background', '');
-      }, 2500);
-    }
-    return;
-  }
-
-  if (stateAnalyze == 'grep') {
-    var regex = new RegExp("info depth " + staticSkill + " seldepth .*? pv (.*)");
-    var seldepthMatch = regex.exec(eventStr);
-    if (seldepthMatch) {
-      console.log('Analyze entry match.');
-      var moves = seldepthMatch[1].split(' ');
-      stateAnalyzeMatch = '';
-      for (var i = 0, len = 5; i < len; i++) {
-        stateAnalyzeMatch += (i + 1) + '. ' + moves[i] + ' ';
-      }
-    };
-    var regex = new RegExp("bestmove .*");
-    var bestmoveMatch = regex.exec(eventStr);
-    if (bestmoveMatch) {
-      $('#game-analyze-string').text(stateAnalyzeMatch).removeClass('hidden');
+  for (var i = 0; i < 2; i++) {
+    var result = game.validate();
+    if (result.status === 'illegal') {
+      gameEnd = true;
       stateAnalyze = false;
-      $('#btn-analyze').removeClass('disabled loading');
-    }
-    return;
-  }
+      stateHint = false;
+      positions.checkmate = true;
 
-  if ((eventStr.indexOf('bestmove') + 1) && stateAnalyze == 'done') {
-    stateAnalyze = false;
-    return;
-  }
-
-  if (eventStr.indexOf('bestmove') + 1) {
-    $('#board-positions-data').text('Tactical solution: ' + event.data);
-  }
-
-  if (!stateAnalyze && !stateHint) {
-    var match = eventStr.match(/^bestmove ([a-h][1-8])([a-h][1-8])([qrbk])?/);
-    if (match) {
-      console.log('Match and move.');
-      stopTimer();
-      var move = game.move({
-        from: match[1],
-        to: match[2],
-        promotion: match[3]
-      });
-      console.log(game.fen());
-      board.position(game.fen());
-      checkPositions('player');
-      checkAnalyzeOption();
-
-      if (game.history().length > 0) {
-        $('#btn-take-back').removeClass('disabled');
+      if (player === 'player') {
+        dumpLog('Checkmate! You lost.');
+        $('#game-positions-data').text('Checkmate! You lost.');
+      } else {
+        dumpLog('Checkmate! You win.');
+        $('#game-positions-data').text('Checkmate! You win.');
       }
 
-      listMoves();
-      startTimer();
+      break;
+    } else if (result.status === 'draw') {
+      gameEnd = true;
+      stateAnalyze = false;
+      stateHint = false;
 
-      gameHistoryAddMove(game.fen());
+      if (result.type === 'stalemate') {
+        positions.stalemate = true;
+        dumpLog('Stalemate! It\'s a draw.');
+        $('#game-positions-data').text('Stalemate! It\'s a draw.');
+      } else if (result.type === 'insufficient_material') {
+        positions.insufficient = true;
+        dumpLog('Insufficient material! It\'s a draw.');
+        $('#game-positions-data').text('Insufficient material! It\'s a draw.');
+      } else if (result.type === 'threefold_repetition') {
+        dumpLog('Threefold repetition! It\'s a draw.');
+        $('#game-positions-data').text('Threefold repetition! It\'s a draw.');
+      } else if (result.type === 'fivefold_repetition') {
+        dumpLog('Fivefold repetition! It\'s a draw.');
+        $('#game-positions-data').text('Fivefold repetition! It\'s a draw.');
+      } else if (result.type === 'seventyfive_moves') {
+        dumpLog('Seventy-five moves rule! It\'s a draw.');
+        $('#game-positions-data').text('Seventy-five moves rule! It\'s a draw.');
+      }
 
+      break;
+    } else if (result.status === 'check') {
+      positions.check = true;
+      dumpLog('Check!');
+
+      if (player === 'player') {
+        $('#game-positions-data').text('Check!');
+      } else {
+        $('#game-positions-data').text('Your opponent is in check!');
+      }
     }
-
   }
 
-};
+  return positions;
+}
+
+// Function to check if analyze option should be enabled
+function checkAnalyzeOption() {
+  var analyzeButton = $('#btn-analyze');
+
+  if (stateAnalyze || stateHint) {
+    analyzeButton.addClass('disabled');
+  } else {
+    analyzeButton.removeClass('disabled');
+  }
+}
+
+// Function to update game end status
+function updateGameEndStatus() {
+  if (game.game_over()) {
+    gameEnd = true;
+    stopTimer();
+
+    if (game.in_draw()) {
+      $('#game-positions-data').text('Draw');
+    } else if (game.in_checkmate()) {
+      $('#game-positions-data').text('Checkmate');
+    }
+  }
+}
+
+// Start the game
+startGame();
